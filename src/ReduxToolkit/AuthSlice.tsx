0137bc884273a490d23c19f12b39b25a896c63d7
Raj1367 <138358960+Raj1367@users.noreply.github.com>
2024-12-07 11:30:39 +0530

    Update AuthSlice.tsx

--- src/ReduxToolkit/AuthSlice.tsx
+++ src/ReduxToolkit/AuthSlice.tsx
@@ -1,50 +1,50 @@
-// authSlice.ts
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { setCookie, getCookie, deleteCookie } from '../Utils/Cookies';
+import { createSlice} from "@reduxjs/toolkit";
+import Cookies from "js-cookie";
 
-interface SerializableUser {
-  uid: string;
-  email: string | null;
-  displayName: string | null;
+interface userCredintials {
+    token: string
+    email: string | null;
 }
 
 interface AuthState {
-  isAuthenticated: boolean;
-  user: SerializableUser | null;
+    isAuthenticated: boolean;
+    user: userCredintials | null;
 }
 
 const initialState: AuthState = {
-  isAuthenticated: false,
-  user: null,
+    isAuthenticated: false,
+    user: null,
 };
 
-// Check cookies on load to initialize state
-const storedUser = getCookie('authUser');
+const storedUser = Cookies.get("userCredintials");
+
 if (storedUser) {
-  const parsedUser: SerializableUser = JSON.parse(storedUser);
-  initialState.isAuthenticated = true;
-  initialState.user = parsedUser;
+    initialState.isAuthenticated = true;
+    const parsedToken = JSON.parse(storedUser)
+    initialState.user = parsedToken
 }
 
-const authSlice = createSlice({
-  name: 'auth',
-  initialState,
-  reducers: {
-    login: (state, action: PayloadAction<SerializableUser>) => {
-      const user = action.payload;
-      setCookie('authUser', JSON.stringify(user), 7); // Set a cookie for 7 days
-      return {
-        ...state,
-        isAuthenticated: true,
-        user,
-      };
-    },
-    logout: () => {
-      deleteCookie('authUser'); // Remove the cookie on logout
-      return initialState;
-    },
-  },
-});
+const AuthSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
 
-export const { login, logout } = authSlice.actions;
-export default authSlice.reducer;
+        login: (state, action) => {
+            const userData = JSON.stringify(action.payload)
+            Cookies.set("firebaseUserToken", userData, { expires: 7, secure: true })
+            return {
+                ...state,
+                isAuthenticated: true,
+                userData
+            }
+        },
+
+        logout: () => {
+            Cookies.remove("firebaseUserToken");
+            return initialState;
+        }
+    }
+})
+
+export const { login, logout } = AuthSlice.actions
+export default AuthSlice.reducer
